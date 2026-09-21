072584659bf7ab87db0e8a7f793d97c68a3b3f3c
cwgraham9 <cwgraham9@gmail.com>
2026-09-20 17:30:51 -0700

    D: updated Asteroids repository

--- player.py
+++ player.py
@@ -1,7 +1,15 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS
+)
+
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
@@ -14,7 +22,8 @@ class Player(CircleShape):
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        right = (pygame.Vector2(0, 1).rotate(self.rotation + 90))
+        * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
@@ -27,19 +36,18 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-             self.rotate(-dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
-             self.rotate(dt)
+            self.rotate(dt)
         if keys[pygame.K_w]:
-             self.move(dt)
+            self.move(dt)
         if keys[pygame.K_s]:
-             self.move(-dt)
+            self.move(-dt)
         if keys[pygame.K_SPACE]:
-             self.shoot()
+            self.shoot()
         self.cooldown -= dt
         if self.cooldown < 0:
             self.cooldown = 0
-        
 
     def move(self, dt: float) -> None:
         unit_vector = pygame.Vector2(0, 1)
@@ -54,6 +62,4 @@ class Player(CircleShape):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         shot.velocity = rotated_vector * PLAYER_SHOOT_SPEED
-        self.cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS 
-       
-        
+        self.cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
